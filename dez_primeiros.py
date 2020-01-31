887cdb7e77ce4ad4291d38f95ecbee53adf021b1
esthefesom <60333749+esthefesom@users.noreply.github.com>
2020-01-31 01:14:42 -0300

    Update dez_primeiros.py

--- dez_primeiros.py
+++ dez_primeiros.py
@@ -1,18 +1,18 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as varpandas
+import matplotlib.pyplot as varplot
 
-sxt = pd.read_csv("manchas.csv")
+sxt = varpandas.read_csv("manchas.csv")
 sxt = sxt.head(10)
-plt.style.use('dark_background')
+varplot.style.use('dark_background')
 
 x = sxt.Ano
 y = sxt.manchas
 
 
-plt.plot(x,y, marker = '_')
-plt.title('Taxa de manchas solares em Wolfer')
-plt.xlabel('ano')
-plt.ylabel('mancha')
-plt.grid(True, linewidth=0.3)
+varplot.plot(x,y, marker = '_')
+varplot.title('Taxa de manchas solares em Wolfer')
+varplot.xlabel('ano')
+varplot.ylabel('mancha')
+varplot.grid(True, linewidth=0.3)
 #plt.show()#
-plt.savefig("dez_primeiros.jpg")
+varplot.savefig("dez_primeiros.jpg")
